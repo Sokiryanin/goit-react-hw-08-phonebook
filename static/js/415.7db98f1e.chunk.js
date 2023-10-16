f814f4a0606ecf00ba7638a6d6ac85152ea7ca27
Sokiryanin <105035206+Sokiryanin@users.noreply.github.com>
2023-10-16 21:07:42 +0000

    Deploying to gh-pages from @ Sokiryanin/goit-react-hw-08-phonebook@00ef48e81fd6d629096582e7eac0c6a0a09be0c8 🚀

---
+++ static/js/415.7db98f1e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(184);function o(){return(0,r.jsx)("div",{children:(0,r.jsx)("h2",{children:"Welcome to the phonebook"})})}}}]);
+//# sourceMappingURL=415.7db98f1e.chunk.js.map
